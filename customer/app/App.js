4ac03b31b00608c05f4fc0cd52c12267ed6a40e9
Siris Regmi <sirisregmi@gmail.com>
2019-08-19 23:32:32 +0630

    QR Generated

--- customer/app/App.js
+++ customer/app/App.js
@@ -1,80 +1,62 @@
-//import React, {Component} from 'react';
-//import {Platform, StyleSheet, Text, View} from 'react-native';
-//import {createRootNavigator} from './router'
+import React from 'react';  
+import {StyleSheet, Text, View} from 'react-native';  
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';  
+import Icon from 'react-native-vector-icons/MaterialIcons';  
+import QRCode from 'react-native-qrcode';
+import ProfileScreen from './screens/Profile'
 
-//export default createRootNavigator();
-
-//This is an example of React Native Tab
-import React from 'react';
-import { Icon } from 'react-native-elements';
-//import react in our code.
-//For React Navigation Version 2+
-//import {createStackNavigator, createMaterialTopTabNavigator} from 'react-navigation';
-//For React Navigation Version 3+
-import {
-  createStackNavigator,
-  createMaterialTopTabNavigator,
-  createAppContainer,
-} from 'react-navigation';
-//import Navigator in our project
-
-
-import Bookcase from './screens/Bookcase';
-import Explore from './screens/Explore';
-
-
-
-
-//Making TabNavigator which will be called in App StackNavigator
-//we can directly export the TabNavigator also but header will not be visible
-//as header comes only when we put anything into StackNavigator and then export
-const TabScreen = createMaterialTopTabNavigator(
-  {
-    Home: {
-      screen: Bookcase,
-      navigationOptions: {
-        tabBarLabel: 'Bookcase',
-        tabBarIcon: ({ tintColor }) => <Icon name="open-book" type="entypo" size={28} color={tintColor} />
-      },
-
-    },
-    Offers: { screen: Bookcase },
-    Gifts: { screen: Explore },
-    Account: { screen: Explore }
-  },
-  {
-
+class HomeScreen extends React.Component {  
+  render() {  
+    return (  
+        <View style={styles.container}>  
+          <Text>Home Screen</Text>  
+          
+        </View>  
+    );  
+  }  
+}  
  
-
-    tabBarPosition: 'bottom',
-    swipeEnabled: true,
-    animationEnabled: true,
-    tabBarOptions: {
-      activeTintColor: '#FFFFFF',
-      inactiveTintColor: '#F8F8F8',
-      showIcon:true,
-      showLabel:false,
-      style: {
-        backgroundColor: '#FFFFF',
-      },
-      labelStyle: {
-        textAlign: 'center',
-      },
-      indicatorStyle: {
-        borderBottomColor: '#87B56A',
-        borderBottomWidth: 2,
-      },
-    },
-  }
-);
-
-//making a StackNavigator to export as default
-const App = createStackNavigator({
-  TabScreen: {
-    screen: TabScreen
-  },
-});
-//For React Navigation Version 2+
-//export default App;
-//For React Navigation Version 3+
-export default createAppContainer(App);
+  
+const TabNavigator = createBottomTabNavigator(  
+    {  
+      Home:{  
+        screen:ProfileScreen,  
+        navigationOptions:{  
+          tabBarLabel:'Home',  
+          tabBarIcon:({tintColor})=>(  
+              <Icon name="home" color={tintColor} size={30}/>  
+          )  
+        }  
+      },  
+      Offers: {  
+        screen:HomeScreen,  
+        navigationOptions:{  
+          tabBarLabel:'Offers',  
+          tabBarIcon:({tintColor})=>(  
+              <Icon name="local-offer" color={tintColor} size={25}/>  
+          )  
+        }  
+      }, 
+      Profile: {  
+        screen:HomeScreen,  
+        navigationOptions:{  
+          tabBarLabel:'Account',  
+          tabBarIcon:({tintColor})=>(  
+              <Icon name="person" color={tintColor} size={25}/>  
+          )  
+        }  
+      },  
+    },  
+    {  
+      initialRouteName: "Home"  
+    },  
+);  
+const styles = StyleSheet.create({  
+  container: {  
+    flex: 1,  
+    justifyContent: 'center',  
+    alignItems: 'center'  
+  },  
+});  
+  
+export default createAppContainer(TabNavigator);  
--- customer/app/screens/Profile.js
+++ customer/app/screens/Profile.js
@@ -1,32 +1,87 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class Profile extends Component {
+import { AppRegistry, 
+        StyleSheet, 
+        View, 
+        TextInput, 
+        TouchableOpacity, 
+        Text} from 'react-native';
+import QRCode from 'react-native-qrcode';
+ 
+class ProfileScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      // Default Value of the TextInput
+      valueForQRCode: '',
+      // Default value for the QR Code
+    };
+  }
+  getTextInputValue = () => {
+    // Function to get the value from input
+    // and Setting the value to the QRCode
+    this.setState({ valueForQRCode: this.state.inputValue });
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Bookcase
-        </Text>
+      <View style={styles.MainContainer}>
+
+        
+        <QRCode 
+           
+          value={this.state.valueForQRCode}
+          //Setting the value of QRCode
+          size={250}
+          //Size of QRCode
+          bgColor="#000"
+          
+          //Backgroun Color of QRCode
+          fgColor="#fff"
+          //Front Color of QRCode
+        />
+        <TextInput
+          // Input to get the value to set on QRCode
+          style={styles.TextInputStyle}
+          onChangeText={text => this.setState({ inputValue: text })}
+          underlineColorAndroid="transparent"
+          placeholder="Enter text to Generate QR Code"
+        />
+       
       </View>
     );
   }
 }
 
+AppRegistry.registerComponent('ProfileScreen', () => ProfileScreen);
+
+export default ProfileScreen;
+
 const styles = StyleSheet.create({
-  container: {
+  MainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
     margin: 10,
-  }
+    alignItems: 'center',
+    paddingTop: 40,
+    backgroundColor:"#FFF"
+  },
+  TextInputStyle: {
+    width: '100%',
+    height: 40,
+    marginTop: 20,
+    borderWidth: 1,
+    textAlign: 'center',
+  },
+  button: {
+    width: '100%',
+    paddingTop: 8,
+    marginTop: 10,
+    paddingBottom: 8,
+    backgroundColor: '#F44336',
+    marginBottom: 20,
+  },
+  TextStyle: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 18,
+  },
 });
